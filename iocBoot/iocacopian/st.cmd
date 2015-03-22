c7d075046fc1ddeddc262200401d6d8e7ff05fcc
Stuart B. Wilkins <swilkins@bnl.gov>
2015-03-22 11:02:24 -0400

    ENH : Changed names of PVs

--- iocBoot/iocacopian/st.cmd
+++ iocBoot/iocacopian/st.cmd
@@ -20,7 +20,6 @@ acopian_registerRecordDeviceDriver(pdbbase)
 
 drvAsynIPPortConfigure("Acopian1","10.23.3.61:502",0,1,1)
 drvAsynIPPortConfigure("Acopian2","10.23.3.62:502",0,1,1)
-drvAsynIPPortConfigure("Acopian3","10.23.3.60:502",0,1,1)
 
 #modbusInterposeConfig(const char *portName, 
 #                      modbusLinkType linkType,
@@ -28,19 +27,15 @@ drvAsynIPPortConfigure("Acopian3","10.23.3.60:502",0,1,1)
 
 modbusInterposeConfig("Acopian1",0,0)
 modbusInterposeConfig("Acopian2",0,0)
-modbusInterposeConfig("Acopian3",0,0)
 
-#asynSetTraceIOMask("Acopian3",0,4)   # Enable traceIOHex
-#asynSetTraceMask("Acopian3",0,9)     # Enable traceError and traceIODriver
+#asynSetTraceIOMask("Acopian2",0,4)   # Enable traceIOHex
+#asynSetTraceMask("Acopian2",0,9)     # Enable traceError and traceIODriver
 
 drvModbusAsynConfigure("A1_AI", "Acopian1", 1, 4, 0x0000, 8, 0, 1000, "et-7017-1") 
 drvModbusAsynConfigure("A2_AI", "Acopian2", 1, 4, 0x0000, 8, 0, 100, "et-7017-2") 
-drvModbusAsynConfigure("A3_BI", "Acopian3", 0, 1, 0x0000, 2, 0, 100, "webrelay") 
-drvModbusAsynConfigure("A3_BO", "Acopian3", 0, 5, 0x0000, 1, 0, 100, "webrelay") 
 
 ## Load record instances
 dbLoadTemplate("$(TOP)/db/et-7017.substitutions")
-dbLoadRecords("$(TOP)/db/webrelay.db","INPORT=A3_BI,OUTPORT=A3_BO,Sys=XF:23ID1-ES,Dev={Acopian}")
 
 iocInit()
 
